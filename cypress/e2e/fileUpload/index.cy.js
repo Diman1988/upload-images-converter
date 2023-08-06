62b809adc4998278bac64b6003fcae641ce4b529
Diman1988 <diman7988@gmail.com>
2023-08-06 13:48:23 +0200

    Cypress tests eslint fixes

--- cypress/e2e/fileUpload/index.cy.js
+++ cypress/e2e/fileUpload/index.cy.js
@@ -1,59 +1,59 @@
-import "cypress-file-upload";
-import { imageConvert } from "./../../../src/index";
+import 'cypress-file-upload';
+import { imageConvert } from './../../../src/index';
 
-describe("Image File Upload Tests", () => {
+describe('Image File Upload Tests', () => {
   beforeEach(() => {
-    cy.visit("cypress/fixtures/index.html");
+    cy.visit('cypress/fixtures/index.html');
   });
 
-  it("should handle single uploaded image file correctly", () => {
+  it('should handle single uploaded image file correctly', () => {
     // Upload a single image file
-    cy.get("#fileInput").attachFile("image1_vertical.jpg");
+    cy.get('#fileInput').attachFile('image1_vertical.jpg');
 
     // Call the imageConvert function
     cy.window().then((win) => {
-      const fileInput = win.document.getElementById("fileInput");
+      const fileInput = win.document.getElementById('fileInput');
       cy.wrap(imageConvert({ files: fileInput.files })).then(
         (convertedFiles) => {
           // Add assertions for the converted files
           // For example, check the type or dimensions of the image
-          expect(convertedFiles[0].type).to.equal("image/webp");
+          expect(convertedFiles[0].type).to.equal('image/webp');
           // Add other checks as needed
-        }
+        },
       );
     });
   });
 
-  it("should handle multiple uploaded image files correctly", () => {
+  it('should handle multiple uploaded image files correctly', () => {
     // Upload multiple image files
-    cy.get("#fileInput").attachFile([
-      "image1_vertical.jpg",
-      "image2_vertical.jpg",
+    cy.get('#fileInput').attachFile([
+      'image1_vertical.jpg',
+      'image2_vertical.jpg',
     ]);
 
     // Call the imageConvert function
     cy.window().then((win) => {
-      const fileInput = win.document.getElementById("fileInput");
+      const fileInput = win.document.getElementById('fileInput');
       cy.wrap(imageConvert({ files: fileInput.files })).then(
         (convertedFiles) => {
           // Add assertions for the converted files
           // For example, check the type or dimensions of the images
           expect(convertedFiles.length).to.equal(2);
-          expect(convertedFiles[0].type).to.equal("image/webp");
-          expect(convertedFiles[1].type).to.equal("image/webp");
+          expect(convertedFiles[0].type).to.equal('image/webp');
+          expect(convertedFiles[1].type).to.equal('image/webp');
           // Add other checks as needed
-        }
+        },
       );
     });
   });
 
-  it("should handle the situation of uploading a file with an invalid format", () => {
+  it('should handle the situation of uploading a file with an invalid format', () => {
     // Upload a file with an invalid format (e.g., txt file instead of an image)
-    cy.get("#fileInput").attachFile("invalid.txt");
+    cy.get('#fileInput').attachFile('invalid.txt');
 
     // Call the imageConvert function
     cy.window().then((win) => {
-      const fileInput = win.document.getElementById("fileInput");
+      const fileInput = win.document.getElementById('fileInput');
 
       const promise = imageConvert({ files: fileInput.files });
 
@@ -61,33 +61,33 @@ describe("Image File Upload Tests", () => {
       return promise.then(
         () => {
           throw new Error(
-            "Expected promise to be rejected but it was resolved."
+            'Expected promise to be rejected but it was resolved.',
           );
         },
         (error) => {
           expect(error.message).to.equal(
-            "Invalid image types detected: text/plain. Supported types are: image/webp, image/jpg, image/jpeg, image/png"
+            'Invalid image types detected: text/plain. Supported types are: image/webp, image/jpg, image/jpeg, image/png',
           );
           return null; // Чтобы вернуться из ошибки и продолжить цепочку
-        }
+        },
       );
     });
   });
 
-  it("should handle if no files selected", () => {
+  it('should handle if no files selected', () => {
     // Upload a single image file
-    cy.get("#fileInput");
+    cy.get('#fileInput');
 
     // Call the imageConvert function
     cy.window().then((win) => {
-      const fileInput = win.document.getElementById("fileInput");
+      const fileInput = win.document.getElementById('fileInput');
       cy.wrap(imageConvert({ files: fileInput.files })).then(
         (convertedFiles) => {
           // Add assertions for the converted files
           // For example, check the type or dimensions of the image
           expect(convertedFiles.length).to.equal(0);
           // Add other checks as needed
-        }
+        },
       );
     });
   });
--- cypress/e2e/imageFormatting/index.cy.js
+++ cypress/e2e/imageFormatting/index.cy.js
@@ -1,54 +1,54 @@
-import "cypress-file-upload";
-import { imageConvert } from "./../../../src/index";
+import 'cypress-file-upload';
+import { imageConvert } from './../../../src/index';
 
-describe("Image Formatting Test", () => {
+describe('Image Formatting Test', () => {
   beforeEach(() => {
-    cy.visit("cypress/fixtures/index.html");
-    cy.fixture("image1_vertical.jpg", "base64").as("image1");
-    cy.fixture("image2_vertical.jpg", "base64").as("image2");
+    cy.visit('cypress/fixtures/index.html');
+    cy.fixture('image1_vertical.jpg', 'base64').as('image1');
+    cy.fixture('image2_vertical.jpg', 'base64').as('image2');
   });
 
   afterEach(() => {
     cy.window().then((win) => {
-      const img1 = win.document.getElementById("demo-image1");
-      img1.src = "";
-      img1.style.display = "none";
+      const img1 = win.document.getElementById('demo-image1');
+      img1.src = '';
+      img1.style.display = 'none';
     });
 
     cy.window().then((win) => {
-      const img2 = win.document.getElementById("demo-image2");
-      img2.src = "";
-      img2.style.display = "none";
+      const img2 = win.document.getElementById('demo-image2');
+      img2.src = '';
+      img2.style.display = 'none';
     });
   });
 
-  it("should handle single uploaded image file correctly", () => {
-    cy.get("@image1").then((image1) => {
-      cy.get("#fileInput").attachFile({
+  it('should handle single uploaded image file correctly', () => {
+    cy.get('@image1').then((image1) => {
+      cy.get('#fileInput').attachFile({
         fileContent: image1,
-        fileName: "image1_vertical.jpg",
-        mimeType: "image/jpg",
-        encoding: "base64",
+        fileName: 'image1_vertical.jpg',
+        mimeType: 'image/jpg',
+        encoding: 'base64',
       });
 
       cy.window().then((win) => {
         // Access the fileInput in the window context
-        const fileInput = win.document.getElementById("fileInput");
+        const fileInput = win.document.getElementById('fileInput');
 
         cy.wrap(imageConvert({ files: fileInput.files })).then(
           (convertedFiles) => {
             expect(convertedFiles.length).to.equal(1);
-            expect(convertedFiles[0].type).to.equal("image/webp");
+            expect(convertedFiles[0].type).to.equal('image/webp');
 
             // Set the converted image as the source for #demo-image
             const objectUrl = URL.createObjectURL(convertedFiles[0]);
 
-            const img1 = win.document.getElementById("demo-image1");
+            const img1 = win.document.getElementById('demo-image1');
             img1.src = objectUrl;
-            img1.style.display = "block";
+            img1.style.display = 'block';
 
-            cy.get("#demo-image1")
-              .should("be.visible")
+            cy.get('#demo-image1')
+              .should('be.visible')
               .and(($img) => {
                 // your assertions here...
                 expect($img[0].complete).to.be.true;
@@ -60,40 +60,40 @@ describe("Image Formatting Test", () => {
                 expect($img[0].naturalWidth).to.equal($img[0].width); // Ensure the image is displayed at its natural size
                 expect($img[0].naturalHeight).to.equal($img[0].height); // Ensure the image is displayed at its natural size
               });
-          }
+          },
         );
       });
     });
   });
 
-  it("should handle single uploaded image file correctly with resolution 1000 * 700", () => {
-    cy.get("@image1").then((image1) => {
-      cy.get("#fileInput").attachFile({
+  it('should handle single uploaded image file correctly with resolution 1000 * 700', () => {
+    cy.get('@image1').then((image1) => {
+      cy.get('#fileInput').attachFile({
         fileContent: image1,
-        fileName: "image1_vertical.jpg",
-        mimeType: "image/jpg",
-        encoding: "base64",
+        fileName: 'image1_vertical.jpg',
+        mimeType: 'image/jpg',
+        encoding: 'base64',
       });
 
       cy.window().then((win) => {
         // Access the fileInput in the window context
-        const fileInput = win.document.getElementById("fileInput");
+        const fileInput = win.document.getElementById('fileInput');
 
         cy.wrap(
-          imageConvert({ files: fileInput.files, width: 1000, height: 700 })
+          imageConvert({ files: fileInput.files, width: 1000, height: 700 }),
         ).then((convertedFiles) => {
           expect(convertedFiles.length).to.equal(1);
-          expect(convertedFiles[0].type).to.equal("image/webp");
+          expect(convertedFiles[0].type).to.equal('image/webp');
 
           // Set the converted image as the source for #demo-image
           const objectUrl = URL.createObjectURL(convertedFiles[0]);
 
-          const img1 = win.document.getElementById("demo-image1");
+          const img1 = win.document.getElementById('demo-image1');
           img1.src = objectUrl;
-          img1.style.display = "block";
+          img1.style.display = 'block';
 
-          cy.get("#demo-image1")
-            .should("be.visible")
+          cy.get('#demo-image1')
+            .should('be.visible')
             .and(($img) => {
               // your assertions here...
               expect($img[0].complete).to.be.true;
@@ -110,34 +110,34 @@ describe("Image Formatting Test", () => {
     });
   });
 
-  it("should handle single uploaded image file correctly with resolution 300 * 500", () => {
-    cy.get("@image2").then((image1) => {
-      cy.get("#fileInput").attachFile({
+  it('should handle single uploaded image file correctly with resolution 300 * 500', () => {
+    cy.get('@image2').then((image1) => {
+      cy.get('#fileInput').attachFile({
         fileContent: image1,
-        fileName: "image2_vertical.jpg",
-        mimeType: "image/jpg",
-        encoding: "base64",
+        fileName: 'image2_vertical.jpg',
+        mimeType: 'image/jpg',
+        encoding: 'base64',
       });
 
       cy.window().then((win) => {
         // Access the fileInput in the window context
-        const fileInput = win.document.getElementById("fileInput");
+        const fileInput = win.document.getElementById('fileInput');
 
         cy.wrap(
-          imageConvert({ files: fileInput.files, width: 300, height: 500 })
+          imageConvert({ files: fileInput.files, width: 300, height: 500 }),
         ).then((convertedFiles) => {
           expect(convertedFiles.length).to.equal(1);
-          expect(convertedFiles[0].type).to.equal("image/webp");
+          expect(convertedFiles[0].type).to.equal('image/webp');
 
           // Set the converted image as the source for #demo-image
           const objectUrl = URL.createObjectURL(convertedFiles[0]);
 
-          const img1 = win.document.getElementById("demo-image1");
+          const img1 = win.document.getElementById('demo-image1');
           img1.src = objectUrl;
-          img1.style.display = "block";
+          img1.style.display = 'block';
 
-          cy.get("#demo-image1")
-            .should("be.visible")
+          cy.get('#demo-image1')
+            .should('be.visible')
             .and(($img) => {
               // your assertions here...
               expect($img[0].complete).to.be.true;
@@ -154,43 +154,43 @@ describe("Image Formatting Test", () => {
     });
   });
 
-  it("should handle multiple uploaded image files correctly", () => {
-    cy.get("@image1").then((image1) => {
-      cy.get("@image2").then((image2) => {
-        cy.get("#fileInput").attachFile([
+  it('should handle multiple uploaded image files correctly', () => {
+    cy.get('@image1').then((image1) => {
+      cy.get('@image2').then((image2) => {
+        cy.get('#fileInput').attachFile([
           {
             fileContent: image1,
-            fileName: "image1_vertical.jpg",
-            mimeType: "image/jpg",
-            encoding: "base64",
+            fileName: 'image1_vertical.jpg',
+            mimeType: 'image/jpg',
+            encoding: 'base64',
           },
           {
             fileContent: image2,
-            fileName: "image2_vertical.jpg",
-            mimeType: "image/jpg",
-            encoding: "base64",
+            fileName: 'image2_vertical.jpg',
+            mimeType: 'image/jpg',
+            encoding: 'base64',
           },
         ]);
 
         cy.window().then((win) => {
           // Access the fileInput in the window context
-          const fileInput = win.document.getElementById("fileInput");
+          const fileInput = win.document.getElementById('fileInput');
 
           cy.wrap(imageConvert({ files: fileInput.files })).then(
             (convertedFiles) => {
               expect(convertedFiles.length).to.equal(2);
               convertedFiles.forEach((file, i) => {
-                expect(file.type).to.equal("image/webp");
+                expect(file.type).to.equal('image/webp');
 
                 // Set the converted image as the source for #demo-image
                 const objectUrl = URL.createObjectURL(file);
 
                 const img = win.document.getElementById(`demo-image${i + 1}`);
                 img.src = objectUrl;
-                img.style.display = "block";
+                img.style.display = 'block';
 
                 cy.get(`#demo-image${i + 1}`)
-                  .should("be.visible")
+                  .should('be.visible')
                   .and(($img) => {
                     // your assertions here...
                     expect($img[0].complete).to.be.true;
@@ -203,36 +203,36 @@ describe("Image Formatting Test", () => {
                     expect($img[0].naturalHeight).to.equal($img[0].height); // Ensure the image is displayed at its natural size
                   });
               });
-            }
+            },
           );
         });
       });
     });
   });
 
-  it("should handle single uploaded image file correctly with wrong resolution -700 * 1000", () => {
-    cy.get("@image2").then((image1) => {
-      cy.get("#fileInput").attachFile({
+  it('should handle single uploaded image file correctly with wrong resolution -700 * 1000', () => {
+    cy.get('@image2').then((image1) => {
+      cy.get('#fileInput').attachFile({
         fileContent: image1,
-        fileName: "image2_vertical.jpg",
-        mimeType: "image/jpg",
-        encoding: "base64",
+        fileName: 'image2_vertical.jpg',
+        mimeType: 'image/jpg',
+        encoding: 'base64',
       });
 
       cy.window().then((win) => {
         // Access the fileInput in the window context
-        const fileInput = win.document.getElementById("fileInput");
+        const fileInput = win.document.getElementById('fileInput');
 
         // Call the imageConvert function and expect it to throw an error
         imageConvert({ files: fileInput.files, width: -700, height: 1000 })
           .then(() => {
             // The promise should not resolve, so fail the test if it does
-            expect.fail("Expected promise to be rejected but it was resolved.");
+            expect.fail('Expected promise to be rejected but it was resolved.');
           })
           .catch((error) => {
             // Check if the error message matches the expected message
             expect(error.message).to.equal(
-              "Invalid input: correctWidth and correctHeight must be positive numbers"
+              'Invalid input: correctWidth and correctHeight must be positive numbers',
             );
           });
       });
